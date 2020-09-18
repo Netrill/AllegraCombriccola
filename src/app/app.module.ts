243574db9b860acc4fba90b7ec038c233d664f57
Christian <christian.scarselli@gmail.com>
2020-09-18 18:16:39 +0200

    Compilazione di un primo form per l' evento , incompleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MapComponent } from './map/map.component';
+import { EventoComponent } from './evento/evento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    MapComponent
+    MapComponent,
+    EventoComponent
   ],
   imports: [
     BrowserModule,
